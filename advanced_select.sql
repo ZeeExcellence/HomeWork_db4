4f4b8b3825ff622e27fccb656644f160de0e2d69
ZeeExcellence <60572445+ZeeExcellence@users.noreply.github.com>
2023-04-29 22:29:53 +0300

    update
    update

--- advanced_select.sql
+++ advanced_select.sql
@@ -1,51 +1,52 @@
 --1 - Количество исполнителей в каждом жанре.
-SELECT genre_name, COUNT(genre_id) FROM genre
+SELECT genre_name, COUNT(genre_id) FROM genre 
 JOIN genre_performer ON genre.id = genre_performer.genre_id
 GROUP BY genre_name;
 
 
 --2 - Количество треков, вошедших в альбомы 2019–2020 годов.
-SELECT title_album, COUNT(album_id) FROM album
-JOIN song ON album.id = song.album_id
-GROUP BY title_album
-
+SELECT COUNT(album_id) FROM song  
+JOIN album  ON album.id = song.album_id
+WHERE album_release BETWEEN 2019 AND 2020;
 
 
 --3 - Средняя продолжительность треков по каждому альбому.
-SELECT title_album, ROUND(AVG(duration)) FROM album
+SELECT title_album, ROUND(AVG(duration)) FROM album 
 JOIN song ON song.album_id = album.id
 GROUP BY title_album;
 
 
 --4 - Все исполнители, которые не выпустили альбомы в 2020 году.
-SELECT nick_name FROM performer
-JOIN album_performer ON album_performer.performer_id = performer.id
-JOIN album ON performer.id = album.id
-WHERE album_release != 2020
-GROUP BY nick_name
-HAVING COUNT(*) = 1;
+SELECT nick_name FROM performer 
+WHERE nick_name NOT IN(
+	SELECT nick_name FROM performer
+	JOIN album_performer ON performer.id = album_performer.id
+	JOIN album ON album_performer.id = album.id
+	WHERE album_release = 2020
+	);
 
 
 --5 - Названия сборников, в которых присутствует конкретный исполнитель (выберите его сами).
-SELECT title_collection FROM collection
-JOIN song_collection ON collection.id = song_collection.collection_id
-JOIN song ON song_collection.song_id = song.id
+SELECT title FROM collection
+JOIN song_collection ON collection.id = song_collection.collection_id 
+JOIN song ON song_collection.song_id = song.id 
 JOIN album  ON song.album_id = album.id
 JOIN album_performer ON album.id = album_performer.album_id
 JOIN performer ON album_performer.performer_id = performer.id
-WHERE nick_name ILIKE '%Marcin Przybyłowicz%'
-GROUP BY title_collection
+WHERE nick_name ILIKE '%eminem%'
+GROUP BY title
 HAVING COUNT(*) >= 1;
 
 
 --6 - Названия альбомов, в которых присутствуют исполнители более чем одного жанра.
-SELECT title_album FROM album
-LEFT JOIN album_performer ON album.id = album_performer.album_id
-LEFT JOIN performer ON album_performer.performer_id = performer_id
-LEFT JOIN genre_performer ON performer.id = genre_performer.performer_id
-GROUP BY title_album
-HAVING COUNT(*) != 1;
--- прошу дать обратную связь по 6 запросу, не знаю привильно ли сделал!!!
+SELECT DISTINCT title_album 
+FROM album
+JOIN album_performer ON album.id = album_performer.album_id 
+JOIN performer ON album_performer.performer_id = performer.id
+JOIN genre_performer ON performer.id = genre_performer.performer_id
+GROUP BY title_album, genre_performer.performer_id
+HAVING COUNT(*) > 1;
+
 
 
 --7 - Наименования треков, которые не входят в сборники.
@@ -57,19 +58,19 @@ WHERE collection.id IS NULL;
 
 --8 - Исполнитель или исполнители, написавшие самый короткий по продолжительности трек, — теоретически таких треков может быть несколько.
 SELECT nick_name FROM performer
-LEFT JOIN album_performer ON performer.id = album_performer.performer_id
-LEFT JOIN album ON album_performer.id = album_id
-LEFT JOIN song ON album.id = song.album_id
-GROUP BY nick_name
-HAVING COUNT(*) != MIN(duration)
-LIMIT 5;
--- прошу дать обратную связь по 8 запросу, не знаю привильно ли сделал!!! как должно получится?
--- только Имя исполнителя или ещё название трека?
+JOIN album_performer ON performer.id = album_performer.performer_id  
+JOIN album ON album_performer.album_id = album.id
+JOIN song ON album.id = song.album_id
+WHERE duration = (
+	SELECT MIN(duration) FROM song);
 
 
 --9 - Названия альбомов, содержащих наименьшее количество треков.
-SELECT title_album, count(song.album_id) AS quantity FROM album
-LEFT JOIN song ON album.id = song.album_id
-WHERE album.id  IN (song.album_id)
-GROUP BY title_album
-ORDER BY quantity ASC;
+SELECT title_album FROM album
+JOIN song ON album.id = song.album_id
+GROUP BY album.title_album
+HAVING count(song) = (
+	SELECT count(song.id) FROM song
+	GROUP BY album_id
+	ORDER BY 1
+	LIMIT 1);
